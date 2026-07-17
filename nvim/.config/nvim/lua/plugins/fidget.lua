e83d1786f7638a1c4859b94b7a0dae6fc69664f2
pox <iampoxel6@gmail.com>
2026-07-17 18:16:31 +0330

    feat(lualine): enabled lualine and replaced fidget.nvim with it.

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -1,5 +1,5 @@
 return {
 	"j-hui/fidget.nvim",
-	opts = {
-	}
+	enabled = false,
+	opts = {},
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,58 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	-- enabled = false,
-	dependencies = { 'nvim-tree/nvim-web-devicons' }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+				refresh_time = 16, -- ~60fps
+				events = {
+					"WinEnter",
+					"BufEnter",
+					"BufWritePost",
+					"SessionLoadPost",
+					"FileChangedShellPost",
+					"VimResized",
+					"Filetype",
+					"CursorMoved",
+					"CursorMovedI",
+					"ModeChanged",
+				},
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = { "filename" },
+			lualine_x = { "fileformat" },
+			lualine_y = { "lsp_status" },
+			lualine_z = { "diff" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	},
 }
