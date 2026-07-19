720390b10333cb00ef5ed7784e318a7d3fe35072
pox <iampoxel6@gmail.com>
2026-07-19 16:19:36 +0330

    fix(blink): blink config now sit in plugin and removed it config entry

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -3,5 +3,35 @@ return {
 	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = { "L3MON4D3/LuaSnip" },
 	version = "1.*",
-	opts = require("config.blink"),
+	opts = {
+		keymap = {
+			preset = "default",
+			["<C-x><C-o>"] = { "show_and_insert_or_accept_single" },
+			["<C-q>"] = { "show", "show_documentation", "hide_documentation" },
+		},
+		completion = {
+			documentation = { window = { scrollbar = false, max_width = 35 } },
+			menu = {
+				max_height = 9,
+				scrollbar = false,
+				draw = {
+					align_to = "label",
+					columns = { { "label", gap = 3 }, { "kind_icon" }, { "kind" } },
+					components = {
+						label = { ellipsis = true, width = { fill = false, min = 30, max = 35 } },
+					},
+				},
+			},
+		},
+		sources = {
+			default = { "lazydev", "lsp", "path", "buffer" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
+		},
+	},
 }
