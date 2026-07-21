ffa7c9f6bcbaa5505952755f06b71c1305bf2a10
pox <iampoxel6@gmail.com>
2026-07-21 19:46:52 +0330

    add(options): new options from teej dotfiles like smartcase and fo-table(fire).

--- nvim/.config/nvim/lua/core/option.lua
+++ nvim/.config/nvim/lua/core/option.lua
@@ -1,22 +1,36 @@
 vim.loader.enable()
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.signcolumn = "yes:1"
-vim.o.winborder = "rounded"
-vim.o.scrolloff = 10
-vim.o.sidescrolloff = 10
-vim.o.wrap = false
 vim.g.mapleader = " "
-vim.g.netrw_banner = false
-vim.o.swapfile = false
-vim.o.splitright = true
-vim.o.splitbelow = true
-vim.o.pumborder = "rounded"
-vim.o.pumblend = 0
-vim.o.pumheight = 8
-vim.o.pumwidth = 50
-vim.o.pummaxwidth = 55
 
+vim.opt.inccommand = "split"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+
+vim.opt.signcolumn = "yes:1"
+
+vim.opt.winborder = "rounded"
+vim.opt.pumborder = "rounded"
+vim.opt.pumblend = 0
+vim.opt.pumheight = 8
+vim.opt.pumwidth = 50
+vim.opt.pummaxwidth = 55
+
+vim.opt.wrap = false
+vim.opt.swapfile = false
+
+vim.opt.formatoptions:remove("o")
+vim.opt.formatoptions:append("q")
+
+vim.g.netrw_banner = false
