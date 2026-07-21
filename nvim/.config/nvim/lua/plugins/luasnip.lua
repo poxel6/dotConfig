db1e0ce42ac997f7579ffe81aa6bacec4a358c28
pox <iampoxel6@gmail.com>
2026-07-21 19:48:17 +0330

    feat(luasnip): add some config options for luasnip.

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -2,8 +2,14 @@ return {
 	"L3MON4D3/LuaSnip",
 	lazy = true,
 	event = "InsertEnter",
-	config = function()
-		require("luasnip").setup()
+	config = function ()
+		local ls = require("luasnip")
+		ls.setup()
+		ls.config.set_config({
+			history = true,
+			updateevents = "TextChanged,TextChangedI",
+			override_builtin = true
+		})
 		require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/lua/snippets/" })
-	end,
+	end
 }
